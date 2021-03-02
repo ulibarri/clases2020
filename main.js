54194df6594e456638d0ac1f24b56f097ef025ca
ErodStar <erodriguez43@ucol.mx>
2021-03-01 21:00:12 -0600

    PRÁCTICA #1

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('Hola mundo!');
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green('Hello world!'));
+console.log(chalk.blue.bgRed.bold('Hello world!'));
