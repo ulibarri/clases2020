34093ea8147a0deefca06e802e33eb88d5b72847
Jonathar26 <jrodriguez110@ucol.mx>
2021-03-01 21:34:49 -0600

    Practica #1

---
+++ app.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.red('Hello world!'));
+console.log(chalk.cyan('Hello world!'));
+console.log(chalk.yellow.bgRed.bold('Hello world!'));
--- asignatura4/main.js
+++ asignatura4/main.js
@@ -1,1 +1,8 @@
-console.log ('Hola mundo')
+console.log ('Hola mundo');
+
+
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green('Hello world!'));
+console.log(chalk.blue.bgRed.bold('Hello world!'));
