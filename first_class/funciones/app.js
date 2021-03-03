2181f1d69701bb953da9fa1f495f9dd5ecf4f345
BrandonVG <54459465+BrandonVG@users.noreply.github.com>
2021-03-03 16:59:54 -0600

    Practica 3

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -3,8 +3,15 @@ greet(); //invoke
 
 let logGreeting = fn => fn();
 
-logGreeting(greet);
+logGreeting(function () {console.log("Hello from a function created on the fly");});
+
 let greetMe = () => console.log("Hello from the function expression");
 
 greetMe();
+
 logGreeting(greetMe);
+
+let logGreeting1 = (miNombre, miColor) => {
+    console.log(`Hola ${miNombre} buenos dias! Tu color favorito es el ${miColor}`);
+}
+logGreeting1("Brandon","morado")
