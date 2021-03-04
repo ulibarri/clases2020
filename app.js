73faa77f87efc5c25c75505db0a880af7ea7e3e2
Luis Alberto Solis Gonzalez <43658532+Alber321@users.noreply.github.com>
2021-03-03 21:00:53 -0600

    FInal Practica2

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
+/*
 function greet() {
     console.log("QuePez");
 }
 greet();
+
+//functions as paameters
+function logGreeting(fn){
+    fn()
+};
+logGreeting(greet);
+
+//function explresion
+ let greetMe = function (){
+     console.log("Hell0 from the function expression")
+ }
+ greetMe();
+
+ logGreeting(greetMe);
+ //Fucntions are first class, pass it as a parameter
+*/
+/*
+function greet(){
+    console.log("Hello there");
+}
+greet();
+
+let logGreeting = (funct1) => funct1()
+    logGreeting(greet)
+    logGreeting(function (){
+        console.log("Hello from the function expression2")
+    })
+*/
+let saludo = (nombre, tratamiento) => console.log("Hola, buen dia " + tratamiento + " " + nombre)
+saludo("Luis", "sr");
