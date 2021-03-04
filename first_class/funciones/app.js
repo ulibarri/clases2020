c15818f530ecde4565b205f5388fed93f2b3c0d9
Juan Manuel Michel Vargas <juan.michel16@outlook.com>
2021-03-03 19:41:20 -0600

    Terminando practica 2 y 3

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -35,3 +35,11 @@ let greetMe = () => console.log('Hello from the function expression');
 greetMe();
 
 logGreeting(greetMe);
+
+
+
+//Refactorizando con function expression created on the fly ==========================================================================
+let logGreeting2 = miParametro => miParametro();
+logGreeting2(() => {
+    console.log(`Hello from function expression created on the fly`);
+});
---
+++ first_class/modules/greet.js
@@ -0,0 +1,4 @@
+console.log('Hello world from the module');
+
+let greet = () => console.log(`Hello world from the module`);
+greet();
