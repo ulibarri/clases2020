348c96f0352d92057597ab168c63b83ef298a546
Luis Alberto Solis Gonzalez <43658532+Alber321@users.noreply.github.com>
2021-03-03 21:44:09 -0600

    Practica3

---
+++ Funciones/app.js
@@ -0,0 +1,56 @@
+/*
+function greet() {
+    console.log("QuePez");
+}
+greet();
+
+//functions as paameters
+function logGreeting(fn){
+    fn()
+};
+logGreeting(greet);
+
+//function explresion
+ let greetMe = function (){
+     console.log("Hell0 from the function expression")
+ }
+ greetMe();
+
+ logGreeting(greetMe);
+ //Fucntions are first class, pass it as a parameter
+*/
+/*
+function greet(){
+    console.log("Hello there");
+}
+greet();
+
+let logGreeting = (funct1) => funct1()
+    logGreeting(greet)
+    logGreeting(function (){
+        console.log("Hello from the function expression2")
+    })
+*/
+/*
+let saludo = (nombre, tratamiento) => console.log("Hola, buen dia " + tratamiento + " " + nombre)
+saludo("Luis", "sr");
+*/
+/*
+function greet() {
+    console.log("Hell0");
+}
+let logGreeting = (miParametro) => miParametro();
+
+logGreeting(greet);
+*/
+
+let logGreeting = (miParametro) => miParametro();
+logGreeting (function (){
+    console.log("Hello from a function craeted on the fly");
+});
+
+//Interpolacion de String "clasica"
+let logGreeting1 = (miNombre, miColor) => {
+    console.log("Hola " + miNombre + " Buenos dias! tu color favorito es el " + miColor);
+}
+logGreeting1("Luis","Morado");
