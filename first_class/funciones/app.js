fb9166a591dd095ee704d1c4c3582193d1c81582
Bastian Natael Hernández Corona <bhernandez20@ucol.mx>
2021-03-05 15:05:02 -0600

    Funciones terminadas

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -3,9 +3,17 @@ function greet(){
 }
 
 let logGreeting=(miParametro)=>  miParametro();
-logGreeting(greet);
-let greetMe=function (){
+
+logGreeting(function(){
+    console.log('Hello from a function created on the fly')
+});
+
+let logGreeting1=(miNombre,miColor)=>{
+    console.log("Hola "+miNombre+" Buenos días! Tu color favorito es el "+miColor)
+}
+logGreeting1("Bastian","Azul")
+/*let greetMe=function (){
     console.log('Hello from the funcion expression');
 }
 greetMe();
-logGreeting(greetMe);
+logGreeting(greetMe);*/
