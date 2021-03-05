9feca7049e55babeb443f29703c86f88b24912a4
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2021-03-05 16:24:07 -0600

    Agrega la primera parte de interpolación de strings

--- first_class/funciones/app.js
+++ first_class/funciones/app.js
@@ -7,7 +7,19 @@ greet(); //invoke
 //functions as parameters
 function logGreeting(fn) {
     fn();
-};
+};*/
+
+
+function greet() {
+    console.log('Hello'); 
+}
+
+greet()
+
+let logGreeting = (miParametro) => miParametro();
+logGreeting(function () {
+    console.log('Hello from a function created on the fly')
+})
 
 logGreeting(greet);
 //estamos pasando la función greet
@@ -17,12 +29,13 @@ logGreeting(greet);
 let greetMe = function () {
     console.log('Hello from the function expression')
 }
+
 greetMe();
 
 logGreeting(greetMe)
 //functions are first class, pass it as a parameter
-*/
 
+/*
 let greet = () => console.log('Hello')
 greet()
 
@@ -31,3 +44,11 @@ logGreeting(greet)
 
 let greetMe = () => console.log('Hello from the function expression')
 greetMe()
+
+*/
+
+//Interpolación de strings "Clásica"
+let logGreeting1 = (miNombre, miColor) =>{
+    console.log("Hola" + miNombre + " buenos días! Tu color favorito es el " + miColor)
+}
+logGreeting1("Alfredo", "azul celeste")
