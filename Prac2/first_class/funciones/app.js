7575a6d6e3d6bb74c1525667cbedc72715a4c771
Alberto Rosales <arosales7@ucol.mx>
2021-03-05 21:10:22 -0600

    App

--- Prac2/first_class/funciones/app.js
+++ Prac2/first_class/funciones/app.js
@@ -2,3 +2,15 @@ function greet() {
     console.log('Hello');
 }
 greet(); //invoke
+
+function logGreeting(fn) {
+    fn();
+};
+logGreeting(greet);
+
+let greetMe = function () {
+    console.log('Hello from the function expression');
+}
+greetMe();
+
+logGreeting(greetMe);
