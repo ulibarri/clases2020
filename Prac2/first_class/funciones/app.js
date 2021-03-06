b7242f13df503951f6d843b721018d5ac887ed3d
Alberto Rosales <arosales7@ucol.mx>
2021-03-05 21:21:15 -0600

    AppES6

--- Prac2/first_class/funciones/app.js
+++ Prac2/first_class/funciones/app.js
@@ -1,4 +1,4 @@
-function greet() {
+/*function greet() {
     console.log('Hello');
 }
 greet(); //invoke
@@ -14,3 +14,22 @@ let greetMe = function () {
 greetMe();
 
 logGreeting(greetMe);
+*/
+//------------------------ES6-----------------------------
+
+let greet = () => {
+    console.log('Hello');
+}
+greet();
+let logGreeting = (fn) => {
+    fn();
+}
+logGreeting(greetArrowFunction);
+let greetMe = () => {
+    console.log('Hello from the function expression');
+}
+greetMe();
+logGreeting(greetMe);
+let miFunction = (something) => {
+    console.log(something);
+}
