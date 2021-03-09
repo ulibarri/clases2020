caf8aa44d56cb94fd10c35ee347036d6b4e5f995
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-03-09 10:42:31 -0600

    parte 8

--- objects/app.js
+++ objects/app.js
@@ -3,9 +3,16 @@ let persona = {
     appellido: 'Villarreal',
     getName: function (){
         return `${this.number} ${this.appellido}` 
+    },
+    domicilio:{
+        calle: 'tulipanes #33',
+        centro:'Bugambilias',
+        cp: 28979,
+        municipio: 'villade alvarez'
     }
 }
 console.log(persona)
 console.log(persona.nombre)
 console.log(persona.appellido)
 
+console.log(persona.domicilio)
