8805392854e3a64af16338171e4d58566355836f
PaAdRaAg <pramos12@hotmail.com>
2021-03-09 11:51:57 -0600

    P4 terminada

---
+++ objectsP#4/app.js
@@ -0,0 +1,27 @@
+let persona = {
+    nombre: "Carlos",
+    apellido: "Ulibarri",
+    getName: function (){
+        return `${this.nombre} ${this.apellido}`
+    },
+    domicilio: {
+        calle: "Madero #144",
+        colonia: "Centro",
+        cp: 28000,
+        municipio: "Colima",
+    },
+    getDom: function (){
+        return `${this.domicilio.calle}, Colonia: ${this.domicilio.colonia}, CP: ${this.domicilio.cp}, ${this.domicilio.municipio}`
+    }
+};
+
+
+
+console.log(persona.getDom());
+
+/*
+console.log(persona.domicilio);
+console.log(persona.getName());
+console.log(persona.nombre);
+console.log(persona.apellido);
+*/
