0945d5d1adf79428c99d79b24a727fdc0d34ddb0
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-03-09 11:22:56 -0600

    parte 5

--- patrones/app.js
+++ patrones/app.js
@@ -1,5 +1,9 @@
 let greet = require('./greet1')
 greet()
 //let greet2 = require('./greet2').greet
-let greet2a = require('./greet2')
+let greet2 = require('./greet2')
 greet2()
+
+const greet3 = require('./greet3')
+console.log(greet3)
+greet3.greet()
