1912866f6533360ccd5326823bad8dbd7c1231be
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-03-09 11:31:01 -0600

    final pract 5
    lo que pasa es que esta mandando un valor a la memoria donde esta guardao el documento

--- patrones/app.js
+++ patrones/app.js
@@ -1,5 +1,5 @@
-let greet = require('./greet1')
-greet()
+//let greet = require('./greet1')
+//greet()
 //let greet2 = require('./greet2').greet
 let greet2 = require('./greet2')
 greet2()
@@ -7,3 +7,6 @@ greet2()
 const greet3 = require('./greet3')
 console.log(greet3)
 greet3.greet()
+greet3.greeting = 'hello from the app'
+let greet3b = require('./greet3')
+greet3b.greet()
