d52206cd4020612e54f4cc6f402bba5ab26d0d71
JoseGabino12 <jmorales18@ucol.mx>
2021-03-10 11:43:50 -0600

    Practica 5 completada

---
+++ patrones/greet3.js
@@ -0,0 +1,9 @@
+function Greetr(){
+    this.greeting = "Hello from the function constructor"
+    this.greet = function() {
+        console.log(this.greeting);
+    }
+
+}
+
+module.exports = new Greetr;
