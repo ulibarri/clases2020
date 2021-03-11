98dace4b49c01e3df703b766662fc0a4215ed414
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-03-10 21:47:34 -0600

    greet2

---
+++ patrones/app.js
@@ -0,0 +1,6 @@
+let greet = require('./greet1');
+//let greet2 = require('./greet2').greet
+let greet2a = require('./greet2')
+greet()
+//greet2()
+greet2a.greet()
---
+++ patrones/greet1.js
@@ -0,0 +1,5 @@
+//modulo que exporta una sola funcion
+let greet = () => {
+    console.log('hello from greet1')
+}
+module.exports = greet
