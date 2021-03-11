249b96cfbab06e8c7ccf16e006030ec1bce670c0
Kenia Villa <kenchi_kenia@hotmail.com>
2021-03-11 14:19:26 -0600

    PRACTICA1

--- practica1/main.js
+++ practica1/main.js
@@ -1,1 +1,5 @@
-console.log('hola')
+console.log('hola');
+const chalk = require ("chalk");
+console.log(chalk.blue("Hello world!"));
+console.log(chalk.blue.bold("Hello world!"));
+console.log(chalk.blue.bgYellow.bold("Hello world!"));
