d9c6b8c5c66ac1c0f59011fd4e599354d4dbb255
IvanJason134 <jestrada1@ucol.mx>
2021-03-11 19:38:59 -0600

    Creacion de los archivos app y greet

---
+++ Patrones/app.js
@@ -0,0 +1,2 @@
+let greet = require('./greet1');
+greet();
---
+++ Patrones/greet1.js
@@ -0,0 +1,5 @@
+//modulo que exporta solo una función
+let greet = () => {
+    console.log('Hello from greet1');
+}
+module.exports = greet;
