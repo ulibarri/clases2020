c74ce94062d64a98fd1945b66427567dac1f7794
IvanJason134 <jestrada1@ucol.mx>
2021-03-11 19:44:13 -0600

    Creacion del archivo greet2

--- Patrones/app.js
+++ Patrones/app.js
@@ -1,2 +1,5 @@
 let greet = require('./greet1');
+let greet2 = require('./greet2').greet;
+
 greet();
+greet2();
---
+++ Patrones/greet2.js
@@ -0,0 +1,4 @@
+//El modulo retorna una propiedad en especifico
+module.exports.greet = function greet() {
+    console.log('Hello from greet2');
+}
