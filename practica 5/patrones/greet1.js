a99a88707fd05d02203b81121a79c5bb1f65e514
Vanessa Covarrubias <acovarrubias10@ucol.mx>
2021-03-13 15:13:15 -0600

    Práctica 5

---
+++ practica 5/patrones/greet1.js
@@ -0,0 +1,6 @@
+//Módulo que exporta una sola función
+let greet = () => {
+    console.log('HELLO from greet1')
+}
+
+module.exports = greet
---
+++ practica 5/patrones/greet2.js
@@ -0,0 +1,5 @@
+//el módulo regresa con una propiedad en específico
+module.exports.greet = function greet() {
+    console.log('Hello from greet2')
+}
+
