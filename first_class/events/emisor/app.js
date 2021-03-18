15087fc69a9f8b3dc97f3a7e83eae76917f30bed
Ulises Ramirez <vwalle.r.m@gmail.com>
2021-03-18 08:36:19 -0600

    Practica #6

--- first_class/events/emisor/app.js
+++ first_class/events/emisor/app.js
@@ -8,3 +8,8 @@ emtr.emit('greet');
 // ! Explicacion:
 // ! Las funciones son agregadas al evento greet a travez del metodo on
 // ! Al llamar al metodo emit y pasar como parametro el evento greet las funciones asignadas a ese evento son llamadas
+
+emtr.on('jump', () => console.log('Someone jumped'));
+console.log(emtr);
+emtr.emit('jump');
+// ! Ahora emit en tu propiedad events tiene un nuevo atributo que corresponde a jum
