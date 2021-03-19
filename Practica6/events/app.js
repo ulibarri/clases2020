4258fc0f85c05e6c8b43c45b04fd976f091dbb96
XimenaV26 <xvelasco1@ucol.mx>
2021-03-19 13:16:47 -0600

    Avance practica 6

---
+++ Practica6/events/app.js
@@ -0,0 +1,11 @@
+let obj = {
+    nombre: "Ximena",
+    edad: 20,
+    apellido: "Velasco"
+}
+
+console.log(obj.nombre);
+console.log(obj['nombre']);
+
+let myKey = 'nombre'
+console.log(obj[myKey]);
