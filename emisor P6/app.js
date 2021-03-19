3fc5a7f3343042d45e17f9f476b226f3febd55da
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2021-03-19 10:32:32 -0600

    La segunda parte de la practica 6

---
+++ emisor P6/app.js
@@ -0,0 +1,21 @@
+var Emitter = require('./emitter');
+
+var emtr = new Emitter()
+
+emtr.on('greet', () =>{
+    console.log('Somewhere, someone said hello')
+})
+
+emtr.on('greet', () => {
+    console.log('A greeting ocurred!')
+})
+
+console.log('Hello!');
+emtr.emit('greet')
+
+emtr.on('jump', () => {
+    console.log('someone jumped!')
+})
+
+console.log(emtr)
+emtr.emit('jump')
