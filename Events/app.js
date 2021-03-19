5325136dbf21f80cadf5a9f3133ecff950fadbd9
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-03-19 13:30:12 -0600

    parte 2 practica 6

--- Events/app.js
+++ Events/app.js
@@ -1,3 +1,4 @@
+/*
 let obj ={
     nombre: "dylan",
     edad: 20,
@@ -5,3 +6,24 @@ let obj ={
 }
 console.log(obj.nombre)
 console.log(obj['nombre'])
+
+
+console.log(obj[myKey])
+//el nombre se vulve a mostrar
+*/
+let myKey = 'nombre'
+let obj ={
+    nombre: "dylan",
+    edad: 20,
+    apellido:"villarreal",
+    keypress: function(){
+        console.log('se ah precionado una tecla')
+    },
+    mouseOver: function() {
+        console.log('el puntero del mouse esta arriba')
+    }
+}
+myKey='keypress'
+console.log(obj[myKey]())
+myKey= 'mouseOver'
+console.log(obj[myKey]())
