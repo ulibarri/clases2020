1e56bed688efeb6446ccbde9799a5853899bc342
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-03-20 17:53:06 -0600

    evento 2

--- events/app.js
+++ events/app.js
@@ -2,6 +2,12 @@ let obj = {
     nombre: 'Edgar',
     edad: 20,
     apellido: 'Gonzalez',
+    keypress: function () {
+        console.log('Se ha presionado una tecla')
+    },
+    mouseOver: function () {
+        console.log('el puntero del mouse esta arriba')
+    }
 }
 // Muestra dos veces nombre
 console.log(obj.nombre)
@@ -10,3 +16,9 @@ console.log(obj['nombre'])
 // En la variable se almacena nombre para despues mostrarla con consolelog
 let myKey = 'nombre'
 console.log(obj[myKey])
+
+// Se crearon 2 funciones y se reemplazan en la variable myKey, al ejecutarlo muestra los contenidos de cada funcion
+myKey = 'keypress'
+console.log(obj[myKey]())
+myKey = 'mouseOver'
+console.log(obj[myKey]())
