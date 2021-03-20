99f0622780e28c941c198d1ae31f64d6ac5a4865
aherredev <aherredev@gmail.com>
2021-03-19 23:55:44 -0600

    Practica 6

---
+++ practica_6/emisor/app.js
@@ -0,0 +1,19 @@
+const Emitter = require('./emitter');
+const emtr = new Emitter();
+emtr.on('greet', () => {
+    console.log('Somewhere, someone said hello');
+});
+
+emtr.on('greet', () => {
+    console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+emtr.emit('greet');
+
+emtr.on('jump', () => {
+    console.log('Someone jumped!');
+});
+
+console.log(emtr);
+emtr.emit('jump');
