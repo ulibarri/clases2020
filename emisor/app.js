8ccf7263b7438bbb8fa1310153c960642e8966cd
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-03-25 11:02:45 -0600

    finalizado

--- emisor/app.js
+++ emisor/app.js
@@ -1,19 +1,20 @@
 let Emitter = require('events') //Con este cambio al ejecutar el codigo muestra la funcion emitter pero con mas detalle
+let config = require('./config') //Inyectamos la dependencia config
 
 let emtr = new Emitter() //Se genera la instancia
 
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
     console.log('somewhere someone said hello')
 })
 
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
     console.log('A greeting occurred')
 })
 
 console.log(emtr) //Nos muestra los eventos de la funcion emitter
 emtr.emit('greet') //Se ejecuta un evento greet de la funcion
 
-emtr.on('jump', () => {
+emtr.on(config.events.JUMP, () => {
     console.log('someone just jumped')
 })
 
