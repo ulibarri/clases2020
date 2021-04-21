1adc788900ce9f748d2b6deefa0850d70fdde870
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2021-04-21 09:25:37 -0500

    Practica 10 Finalizada

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+require('dotenv').config();
+var express = require('express');
+var app = express();
 
-app.listen(3000);
-app.get('/', (req, res) => {
-    res.send('Hello world, this is the root route');
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+   res.send('<html><head><body><h1>Hello world!</h1></body></head></html>') ;
 });
-app.get('/uno', (req, res) => {
-    res.send('Hello world, from route uno');
+app.get('/api', function(req, res) {
+   res.json({firstname: 'Ivan', lastname: 'Godinez'});
 });
-app.get('/prueba', (req, res) => {
-    res.sendFile(path.join(__dirname + '/html/prueba.html'));
-});
+app.get('/person/:id', function(req, res) {
+   res.send('<html><head><body><h1>Persona: ' + req.params.id + '</h1></body></head></html>')
+})
+app.listen(port);
