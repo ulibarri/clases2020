441dbf49007c1b20f65f9695fcf05f02d2d609bb
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2021-04-21 17:30:42 -0500

    Practica 8 Finalizada

---
+++ httpServer/app.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+// http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello world\nMy name is Ivan');;
+// }).listen(1337, 'localhost');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world\nMy name is Ivan');
+}).listen(1337, 'localhost');
+
+/* 
+Al acceder a localhost:1337 encontramos lo que hemos escritor en la linea 5 o 10.
+Si entramos a los headers encontraremos datos básicos de de los requests y responses
+*/
