4044b0c1326c581e29d4c2577e2ba6ba0bd7d35a
PaAdRaAg <pramos12@hotmail.com>
2021-04-22 17:51:16 -0500

    CorregidoLoOlvidadoxd

--- httpServer/app.js
+++ httpServer/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function(req, res){
+http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello wold\n');
 }).listen(1337, '127.0.0.1');
