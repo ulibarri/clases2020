de6d74eb0aafd077929e9ad09e1673ffddb72bdc
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2021-04-23 17:39:15 -0500

    Practica 9

---
+++ practica_9 P9/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res)=> { //route handler
+ res.send('Hello world, this is the root route')
+})
+
+app.listen(3000)
+app.get('/uno', (req, res)=> {//route handler
+    res.send('Hello world, frome route one')
+})
+
+app.get('/prueba', (req, res)=> {//route handler
+    res.send('"Hello world, frome route Prueba')})
