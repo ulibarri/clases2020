ad325501f04ec35bd2b862d311d17a17d5ace31f
Kenia Villa <kenchi_kenia@hotmail.com>
2021-04-23 17:15:23 -0500

    PRACTICA 9

---
+++ practica9/app.js
@@ -0,0 +1,17 @@
+
+const express =  require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res)=> { //router handler
+    res.send('Hello world, this is the root route');
+})
+
+app.listen(3000);
+app.get('/uno', (req, res)=> {
+    res.send('Hellos world, frome route one');
+})
+
+app.get('/prueba', (req, res)=> {
+    res.sendFile(path.join(__dirname + '/index.html')); //('Hello world, frome route prueba');
+})
