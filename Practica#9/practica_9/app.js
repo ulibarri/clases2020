1a90431192810443cd8c23c3e020ecadb02eb71b
PaAdRaAg <pramos12@hotmail.com>
2021-04-22 20:05:38 -0500

    Práctica#9 Termiinada

---
+++ Practica#9/practica_9/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{ //route handler
+    res.send('Hello World, this is the root route');
+});
+
+
+app.listen(3000);
+app.get('/uno', (req, res)=>{ //route handler
+    res.send('Hello World, from route One');
+});
+
+app.get('/prueba', (req, res)=>{ //route handler
+    res.send('<html><head></head><body><h1>Hola desde prueba</h1></body></html>');
+});
