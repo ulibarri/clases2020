6710aa7dc97f89606ed2cccb81bc01d8bd34f9a6
XimenaV26 <xvelasco1@ucol.mx>
2021-04-22 19:20:15 -0500

    practica 9

---
+++ Practica9/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+app.get('/',(req, res)=>{
+    res.send('Hello world, this is the root route')
+})
+app.listen(3000)
+app.get('/uno', (req, res)=>{
+
+    res.send('Hellow World, frome route One')
+})
+
+app.get('/prueba', (req,res)=> { //route handler 
+    res.send('Hello world, from route Prueba')
+
+})
+
+app.get('/Xime', (req,res)=> {
+    res.send('Hola a todos, desde Ximena:)')
+})
