b3861c7d031a1f21216c47c86e3da488109876e3
FelipeRodriguezMX <FelipeRod0201@gmail.com>
2021-04-23 21:18:14 -0500

    practica 9

---
+++ practica_9/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Hello world, this is the root route');
+});
+
+app.listen(3000);
+
+app.get('/uno',(req,res) => {
+    res.send('Hello world, from route one');
+});
+
+app.get('/prueba',(req,res) => {
+    res.send('Hello world, from route prueba');
+});
