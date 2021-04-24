92ae2d55450dd633971aeefbc0fcde52605d5722
Kenia Villa <kenchi_kenia@hotmail.com>
2021-04-23 19:12:54 -0500

    PRACTICA 10

---
+++ practica10/app.js
@@ -0,0 +1,21 @@
+var express = require('express'); //importamos la dependencia
+var app = express(); //declarams una app de express
+
+var port = process.env.PORT || 3000; //setteamos el puerto para que escuche el servidor
+
+//primera ruta (está al nivel de la raiz /), Hello word!
+app.get('/', function (req, res) {
+    res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+})
+
+app.listen(port); //levantar el server y ponerlo a la escucha
+
+//segunda ruta /api, regresando un objeto JSON
+app.get('/api', (req, res) => {
+    res.json({ firstname: 'Kenia', lastname: 'Villa' });
+})
+
+//tercera ruta, recibe un parametro
+app.get('/person/:id', function (req, res) {
+    res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
+})
