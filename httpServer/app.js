470a4fe30fc6fb63eee9d0fcd770ffca3a78e20a
Luis Leyva <lmorales6@ucol.mx>
2021-04-23 21:01:09 -0500

    Lista practica 8

---
+++ httpServer/app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {'Cntent-type': 'text/plain'});
+    res.end('Hello world?\n');
+}).listen(1337, '127.0.0.1');
