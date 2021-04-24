9332164369a04aa221aa724052c616837e8198ea
Luis Leyva <lmorales6@ucol.mx>
2021-04-23 20:54:58 -0500

    Lista practica 10

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+});
+
+app.listen(port);
+
+//segunda ruta
+app.get('/api', function(req, res){
+    res.json({fisrtname: 'James', lastname: 'Bond'})
+});
+
+//tercera ruta
+app.get('/person/:id', function(req, res){
+    res.send('<html><head></head><body><h1>Person:' + req.params.id + '</h1></body></html>');
+});
