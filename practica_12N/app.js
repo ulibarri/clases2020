19ecfbf0d97e24bde4361767a435408a7c84af87
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2021-04-28 13:40:49 -0500

    practica 12

---
+++ practica_12N/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', (req, res, next) =>{
+    console.log('Request Url:' + req.url);
+    next();
+});
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+app.get('/person/:id', (req, res)=>{
+    res.render('person', {ID: req.params.id});
+});
+app.get('/api', (req, res)=>{
+    res.json({firstname: 'John', lastname: 'Connor'});
+});
+
+app.listen(port);
+ //2practica 12
