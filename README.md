77f83837393bb9dcb2880dea4fa54a45a580a68e
QuackDuster <jjimenez@ucol.mx>
2021-04-28 15:09:01 -0500

    Practica 12 Completada

---
+++ README.md
@@ -0,0 +1,9 @@
+# Practica #12
+## Template Engines
+
+Una forma de enviar información que se genera en los “route handlers” de nuestra app, hacia el “front end” es mediante un template engine. Express permite una “conectar” una cantidad considerable de “templates engines” a nuestra aplicación de express.
+Uno de los “template engines” más populares es “EJS”. 
+
+
+**Referencias**
+- [Instrucciones Practica 11](https://docs.google.com/document/d/11WnWhuy92hhCSjjyhLZQfD8KSy8TtiBapa4yrp47-YM/edit)
---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next){
+    console.log('Request Url:' + req.url);
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.render(`index`);
+})
+/*
+app.get('/', function (req, res) {
+    res.send(`<html><head><link href="assets/style.css" type=text/css rel="stylesheet" /></head><body><h1>Hello world!</h1></body></hmtl>`);
+})
+*/
+app.get('/api', function (req, res){
+    res.json({ firstName: 'John', lastName:'Doe'});
+});
+
+app.get('/person/:id', function (req, res){
+    res.render('person', { ID: req.params.id });
+});
+
+app.listen(port);
