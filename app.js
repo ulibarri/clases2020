46f232fd6422e7763a68f84bda20b1c7005d57e9
marioacar <mcarvantes@ucol.mx>
2021-04-29 16:20:49 -0500

    Practica 12

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/',function(req,res,next){
+    console.log('request url:' + req.url);
+    next();
+});
+
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/person/:id',function(req,res){
+    res.render('person',{ID:req.params.id});
+});
+
+app.get ('/api', function(req,res){
+    res.json({firstname:'Jhon', lastname:'Doe'});
+});
+
+
+app.listen(port);
