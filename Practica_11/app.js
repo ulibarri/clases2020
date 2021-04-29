174ab1b82622a509220eda851855ed42ca99a387
Karla Sierra <ksierra@ucol.mx>
2021-04-29 00:42:41 -0500

    Practica 11

---
+++ Practica_11/app.js
@@ -0,0 +1,22 @@
+var express = require('express'); //importamos la dependencia
+var app = express(); //declaramos una app de express
+var port = process.env.PORT || 3000; //setteamos el puerto para que escuche el servidor
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', function (req, res, next) {
+    console.log("Request Url:" + req.url);
+    next();
+});
+//esta al nivel de la raiz /, Hello world!
+app.get('/', function(req, res) {
+    res.send(`<html><head><link href=assets/style.css type=text/css rel=stylesheet></head>
+    <body><h1>Hello World!</h1></body></html>`);
+});
+//recibe un parametro
+app.get('/person/:id', function(req, res) {
+    res.send('<html><head></head><body><h1>Persona: ' + req.params.id + '</h1></body></html>');
+ });
+ //regresa un objeto JSON
+app.get('/api', function(req, res) {
+    res.json({firstname: 'Karla', lastname: 'Sierra'});
+ });
+app.listen(port); //levantar el server y ponerlo a la escucha
