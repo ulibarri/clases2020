e50a3bd81c746eef6b0d2a9b4ebd460d9abf9577
Karla Sierra <ksierra@ucol.mx>
2021-04-29 20:35:05 -0500

    Practica 12

---
+++ Practica_12/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express(); 
+var port = process.env.PORT || 3000; 
+app.set ('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', function (req, res, next) {
+    console.log("Request Url:" + req.url);
+    next();
+});
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/person/:id', function (req, res) {
+    res.render('person', { ID: req.params.id });
+});
+app.get('/api', function(req, res) {
+    res.json({firstname: 'Karla', lastname: 'Sierra'});
+ });
+app.listen(port); 
