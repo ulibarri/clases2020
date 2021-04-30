6efe372a432a56f892700909f826e4f085a151c5
MrSoniccx <yahel_isordia@hotmail.com>
2021-04-29 22:39:10 -0500

    finich

---
+++ practica_12/app.js
@@ -0,0 +1,34 @@
+var express = require('express'); //importamos la dependencia
+var app = express(); //Declaramos una app de express
+var port = process.env.PORT || 3000; //Setteamos el puerto para que escuche el servidor
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set ('view engine', 'ejs');
+
+app.use('/', function(req, res, next) {
+   console.log('Requested Url: '+ req.url);
+   next();
+});
+
+//Primera ruta (esta al nivel de la raiz /), Hello World!
+app.get('/', function(req, res) {
+   res.render('index');
+});
+
+//tercera ruta, recibe un parametro
+app.get('/person/:id', function(req, res) {
+   res.render('person', {ID:req.params.id});
+});
+
+//segunda ruta /api, regresando un objeto JSON
+app.get('/api', function(req, res) {
+   res.json({firstname: 'Leonardo', lastname: 'Isordia'});
+});
+
+
+
+
+app.listen(port); //levantar el server y ponerlo a la escucha
+
+
