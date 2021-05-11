b9df428c38386e2b71146b75583fc07e086fe31f
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-05-10 22:36:40 -0500

    1

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use("/assets", express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.send(
+    `<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world</h1></body></html>`
+  );
+});
+
+app.get("/api", function (req, res) {
+  res.json({ firstname: "John", lastname: "Doe" });
+});
+
+app.get("/person/:edgar", function (req, res) {
+  res.send(
+    "<html><head></head><body><h1>Person: " +
+      req.params.edgar +
+      "</h1></body></html>"
+  );
+});
+
+app.listen(port);
