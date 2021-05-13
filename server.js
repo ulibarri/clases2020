afc90283bce947e252a71c064f99a803984e1aae
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2021-05-13 18:57:32 -0500

    Finalizado

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', (req, res, next) => {
    res.send(`<!DOCTYPE html> <html lang="en"> <head><link rel="stylesheet" href="/assets/style.css"></head><title>Document</title></head><body><h1>Hola mundo</h1><p>Este es un parrafo y su contenido debe ser azul</p></body></html>`);
 });
 
-app.get('/person/:id', (req, res) => {
-   res.render('person', {ID: req.params.id, q: req.query.q});
+app.get('/person/:name', (req, res) => {
+   res.render('person', {name: req.params.name, message: req.query.message, times: req.query.times});
 });
 
 app.listen(port);
