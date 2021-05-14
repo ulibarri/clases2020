7304743f2889edfd184436dbc3a09e1e8b971f0d
Eduardo Daniel López Fletes <54423167+EduardEl19@users.noreply.github.com>
2021-05-14 11:38:35 -0500

    practica 14

---
+++ practica 14/server.js
@@ -0,0 +1,19 @@
+const express = require('express'); //inyeccion de la dependencia
+let app = express()
+let PORT = process.env.PORT || 3000; //definicion del puerto de escucha
+app.use('/assets', express.static(__dirname + '/public'));//contenido estatico
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req, res) =>{
+    res.send(`<!DOCTYPE html> <html lang="en"> <head><link rel="stylesheet" href="/assets/style.css">
+    <title>Document</title> </head> 
+    <body> <h1>Hola mundo </h1> 
+    </body> </html>`)
+})
+
+app.get('/person/:id',(req, res)=>{
+    res.render('person',{ID: req.params.id, Qstr: req.query.message, Qstr: req.query.times});
+});
+
+app.listen(PORT);
