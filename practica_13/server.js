765675e4007a69e3b4861e1b8e389f1c19235013
aherredev <aherredev@gmail.com>
2021-05-14 22:45:28 -0500

    Practica 13

---
+++ practica_13/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (_, res) => {
+   res.send(`<!DOCTYPE html> 
+             <html lang="en">
+             <head><link rel="stylesheet" href="/assets/style.css"><title>Document</title></head>
+             <body><h1>Hola mundo</h1><p>Este es un parrafo y su contenido debe ser azul</p></body>
+             </html>`);
+});
+
+app.get('/person/:id', (req, res) => {
+   res.render('person', {ID: req.params.id, q: req.query.q});
+});
+
+app.listen(port);
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Nota
+Todas las practicas se corren desde el directorio raiz y ejecuta el codigo de la practica para
+evitar crear un nuevo proyecto por clase (solo se reusa el anterior)
