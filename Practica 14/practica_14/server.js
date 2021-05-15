627265a8e2150c12e5a8847d6da0c9c894785e16
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2021-05-14 23:51:37 -0500

    Agrega el inicio de la practica 14

---
+++ Practica 14/practica_14/server.js
@@ -0,0 +1,17 @@
+const express = require('express'); //Importamos la dependencia
+let app = express(); //Declaramos una app de express
+let PORT = process.env.PORT || 3000; // setteamos el puerto para que escuche el servidor
+
+app.use('/assets', express.static(__dirname + '/public'))
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.send('<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" /head><body><h1>Hola mundo!</h1></body></html>')
+})
+
+
+app.get('/person/:id', function (req, res){
+    res.render('person', {ID: req.params.id, Qstr: req.query.qrst});
+})
+
+app.listen(PORT); 
