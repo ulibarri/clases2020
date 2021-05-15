a80fa833304e42d3ba462edc23caaa70ee938cb2
FelipeRodriguezMX <FelipeRod0201@gmail.com>
2021-05-14 19:01:48 -0500

    preactica 13

---
+++ practica_14/server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname+'/public'));
+app.set('view engine', 'ejs');
+app.use('/', function (req,res,next) {
+    console.log(`Reuquest url: ${req.url}`);
+    next();
+})
+
+app.get('/', function(req,res) {
+    res.send(`<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <link href=assets/style.css type=text/css rel=stylesheet />
+                    <title>Document</title>
+                </head>
+                <body>
+                    <h1>Hola mundo</h1>
+                    <p>este es un parrafo y su contenido debe ser azul</p>
+                </body>
+                </html>`);
+});
+
+app.get('/json', function(req,res) {
+    res.json({firstName:'John', lastName:'Doe'});
+});
+
+app.get('/persona/:id', function(req,res) {
+    res.render('persona',{ID: req.params.id, Message: req.query.message, Times: req.query.times});
+});
+
+app.listen(port);
