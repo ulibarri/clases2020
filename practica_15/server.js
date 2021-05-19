47e69417a48b5f93fe60fc6cbd8b24a7698ebc29
BrandonVG <bvadillo0@ucol.mx>
2021-05-19 12:04:12 -0500

    Practica 15 finalizada

---
+++ practica_15/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+let app = express();
+let PORT = process.env.PORT || 3000;
+app.use("/assets", express.static(__dirname + "/public"));
+
+app.use(express.urlencoded({ extended: false}));
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.send(`<!DOCTYPE html> <html lang="en"> <head><link rel="stylesheet" href="/assets/style.css">
+    <title>Document</title> </head>
+    <body> <h1>Hola mundo</h1>
+    <p> Este es un parrafo y su contenido debe ser azul</p></body></html>`);
+});
+
+app.get("/person/:id",(req,res)=>{
+    res.render("person",{ID: req.params.id, Message: req.query.message, Times : req.query.times});
+});
+
+app.get("/student", (req, res) =>{
+    res.render("index");
+});
+
+app.post("/student",(req, res)=>{
+    res.send(`First Name es: ${req.body.fname}, Last Name es: ${req.body.lname}`);
+});
+
+app.listen(PORT);
