a5c7b23bf94e473a33e8d51c69217d991655d1b6
FelipeRodriguezMX <FelipeRod0201@gmail.com>
2021-05-19 21:21:56 -0500

    practica 15 terminada

---
+++ practica_15/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname+'/public'));
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use('/', function (req,res,next) {
+    console.log(`Request url: ${req.url}`);
+    next();
+})
+
+app.get('/', function(req,res) {
+    res.send(`<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <link href=assets/style.css type=text/css rel=stylesheet />
+                    <title>Document</title>
+                </head>
+                <body>
+                    <h1>Hola mundo</h1>
+                    <p>este es un parrafo y su contenido debe ser azul</p>
+                </body>
+                </html>`);
+});
+
+app.get('/json', function(req,res) {
+    res.json({firstName:'John', lastName:'Doe'});
+});
+
+app.get('/persona/:id', function(req,res) {
+    res.render('persona',{ID: req.params.id, Message: req.query.message, Times: req.query.times});
+});
+app.get("/student", (req, res) =>{
+    res.render("index");
+});
+app.post('/student', function (req,res) {
+    res.render(`First name es ${req.body.fname}, y el Last name es ${req.body.lname}`); 
+});
+app.listen(port);
