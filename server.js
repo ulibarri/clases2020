b527532f8f7b07e970e5ccad753941e3cf2819b6
Luis Leyva <lmorales6@ucol.mx>
2021-05-21 18:41:12 -0500

    Lista Practica 15

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send(`<html><head><link href=assets/style.css type=text/css rel="stylesheet" /></head><body><h1>Hello world!</h1><p>Este es un parrafo y su contenido debe ser azul</p></body></html>`);
+});
+
+app.get('/person/:id', (req,res) => {
+    res.render('person', {ID: req.params.id, Qstr: req.query.qr});
+ });
+
+ app.get('/student', (req,res) => {
+    res.render('index');
+ });
+ 
+ app.post('/student', (req,res) => {
+    res.send(`First name: ${req.body.fname}, Last name: ${req.body.lname}`);
+    console.log(req.body)
+ });
+app.listen(port);
