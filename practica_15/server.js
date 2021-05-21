86bfdc5e4d6084d5796bb6a053e0783d97c5c25f
ErodStar <erodriguez43@ucol.mx>
2021-05-21 12:30:56 -0500

    Añadir práctica #15

---
+++ practica_15/server.js
@@ -0,0 +1,30 @@
+
+const express= require('express'); //Inyección de la dependencia
+let app = express();
+let PORT = process.env.PORT || 3000; //definición del puerto de escucha
+
+app.use('/assets', express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: false}));
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.send(`<!DOCTYPE html> <html lang="en"> <head><link rel="stylesheet" href="/assets/style.css">
+    <title>Document</title> </head> 
+    <body> <h1>Hola mundo </h1>
+    </body></html>`)
+})
+
+app.get('/person/:id', (req,res) => {
+    res.render('person', {id: req.params.id, message: req.query.message, times: req.query.times});
+});
+
+app.get('/student', (req,res) => {
+    res.render('index');
+});
+
+app.post('/student', (req,res) => {
+    res.send(`First Name es: ${req.body.fname}, Last Name es: ${req.body.lname}`);
+});
+
+app.listen(PORT);
