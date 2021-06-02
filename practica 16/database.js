2440c88398e0cf0e81fb859ddc1edaf32be21fc0
eduardo343 <54459451+eduardo343@users.noreply.github.com>
2021-06-02 04:03:45 -0500

    Add files via upload

---
+++ practica 16/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import config from "./config";
+
+(async () => {
+  try {
+    const db = await mongoose.connect('mongodb+srv://malan117:malan117@cluster0.jljuu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("Mongodb is connected to", db.connection.host);
+  } catch (error) {
+    console.error(error);
+  }
+})();
