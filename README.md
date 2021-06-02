edceff61a12df56237f777c22ac01a12a02a9ee1
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2021-06-01 22:39:45 -0500

    Finalizado

--- README.md
+++
@@ -1,2 +0,0 @@
-Practica #16
-Escribir una aplicación (server.js) para probar el módulo express y ejs junto con jQuery con lo indicado en el [documento](https://docs.google.com/document/d/1XNiQICVkFuzeHJ5rI3gMNfRnpMJJZWZOF6Sli6_-pEA/edit).
---
+++ main.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const Citas = require('./models/citas.js');
+let app = express();
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    app.get('/add', (req, res) => {
+        Citas.findOne({nombre: 'Iván', ubicacion: 'Centro'}, (err, object) => {
+            if (err) console.error(err);
+            if (object) {
+                res.send('Ya esta añadido');
+                console.log('Ya esta añadido');
+            } else {
+                let newCitas = new Citas({
+                    nombre: 'Iván',
+                    ubicacion: 'Centro',
+                    date: new Date()
+                });
+                newCitas.save().then(() => {
+                    res.send(`Añadido\n${newCitas}`);
+                    console.log(`Añadido\n${newCitas}`);
+                }).catch((err) => console.err(err));
+            }
+        });
+    });
+    app.get('/delete', (req, res) => {
+        Citas.findOne({nombre: 'Iván', ubicacion: 'Centro'}, (err, object) => {
+            if (err) console.error(err);
+            if (!object) {
+                res.send('No existe ningun objeto con esos parametros');
+                console.log('No existe ningun objeto con esos parametros');
+            } else {
+                Citas.deleteOne({
+                    nombre: 'Iván',
+                    ubicacion: 'Centro'
+                }).then(() => {
+                    res.send(`Eliminado\n${object}`);
+                    console.log(`Eliminado\n${object}`);
+                }).catch((err) => console.err(err));
+            }
+        });
+    });
+});
