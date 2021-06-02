a485cfd934ebb6b8482da167eb6c99fcb32462af
Jesús Iván Godínez Martínez <ivano.godinez@gmail.com>
2021-06-02 09:25:42 -0500

    Finalizado
    - Se añadieron estilos
    - Se instalo sass y ejs
    - Se añadió la posibilidad de agregar o eliminar objetos desde los params y el query de la URL

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Practica #17
-Escribir una aplicación (main.js) para usar MongoDB con el módulo mongoose y express con lo indicado en el [documento](https://classroom.google.com/u/1/c/Mjc1NzA4MzQ3Njgy/a/MzUzMjA0OTAwOTg0/details).
+Escribir una aplicación (main.js) para usar MongoDB con el módulo mongoose, express y ejs con lo indicado en el [documento](https://classroom.google.com/u/1/c/Mjc1NzA4MzQ3Njgy/a/MzUzMjA0OTAwOTg0/details).
 
 <p align="center"><a href="https://www.youtube.com/watch?v=Imwk0HtEuGY"><img src=https://img.youtube.com/vi/Imwk0HtEuGY/0.jpg alt="YouTube"/></a></p>
--- main.js
+++ main.js
@@ -4,36 +4,40 @@ const Citas = require('./models/citas.js');
 let app = express();
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-    app.get('/add', (req, res) => {
-        Citas.findOne({nombre: 'Iván', ubicacion: 'Centro'}, (err, object) => {
+    app.use('/assets', express.static(__dirname + '/public'));
+    app.set('view engine', 'ejs');
+    app.get('/add/:nombre', (req, res) => {
+        //En mi caso https://localhost:3001/add/Iván?ubicacion=Centro
+        Citas.findOne({nombre: req.params.nombre, ubicacion: req.query.ubicacion}, (err, object) => {
             if (err) console.error(err);
             if (object) {
-                res.send('Ya esta añadido');
+                res.render('person.ejs', {nombre: req.params.nombre, ubicacion: req.query.ubicacion, evento: 'El objeto ya existe', objeto: object});
                 console.log('Ya esta añadido');
             } else {
                 let newCitas = new Citas({
-                    nombre: 'Iván',
-                    ubicacion: 'Centro',
+                    nombre: req.params.nombre,
+                    ubicacion: req.query.ubicacion,
                     date: new Date()
                 });
                 newCitas.save().then(() => {
-                    res.send(`Añadido\n${newCitas}`);
+                    res.render('person.ejs', {nombre: req.params.nombre, ubicacion: req.query.ubicacion, evento: 'Añadido', objeto: newCitas});
                     console.log(`Añadido\n${newCitas}`);
-                }).catch((err) => console.err(err));
+                }).catch((err) => console.error(err));
             }
         });
     });
-    app.get('/delete', (req, res) => {
-        Citas.findOne({nombre: 'Iván', ubicacion: 'Centro'}, (err, object) => {
+    app.get('/delete/:nombre', (req, res) => {
+        //En mi caso https://localhost:3001/add/Iván?ubicacion=Centro
+        Citas.findOne({nombre: req.params.nombre, ubicacion: req.query.ubicacion}, (err, object) => {
             if (err) console.error(err);
             if (!object) {
-                res.send('No existe ningun objeto con esos parametros');
+                res.render('person.ejs', {nombre: req.params.nombre, ubicacion: req.query.ubicacion, evento: 'El objeto no existe', objeto: object});
                 console.log('No existe ningun objeto con esos parametros');
             } else {
                 Citas.deleteOne(object).then(() => {
-                    res.send(`Eliminado\n${object}`);
+                    res.render('person.ejs', {nombre: req.params.nombre, ubicacion: req.query.ubicacion, evento: 'Eliminado', objeto: object});
                     console.log(`Eliminado\n${object}`);
-                }).catch((err) => console.err(err));
+                }).catch((err) => console.error(err));
             }
         });
     });
