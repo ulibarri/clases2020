defcd5933873e5cbf913f7201a06043e52156fb2
marioacar <mcarvantes@ucol.mx>
2021-06-04 17:45:52 -0500

    practica16

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+let app = express();
+let PORT = process.env.PORT || 3000;
+app.use('/assets',express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs')
+
+
+app.get('/',(req,res)=>{
+    res.send(`<!DOCTYPE html> <htm lang='eng'> <head> <link rel="stylesheet" href="/assets/style.css" type="text/css"/> 
+    <title>Document</title> </head>
+    <b> <h1>Hola Mundo!</h1>`)
+});
+
+app.get('/person/:id', (req,res) => {
+    res.render('person', {ID: req.params.id, Qstr: req.query.qrst})
+});
+
+app.get('/student',(req,res)=>{
+    res.render('index')
+})
+
+app.post('/student', (req,res) =>{
+    res.send(`First name is: ${req.body.Fname}, Last Name is: ${req.body.Lname}`)
+}) 
+
+app.post('/personjson', express.json({type: '*/*'}),(req,res)=>{
+    console.log('El objeto contiene:', (req.body));
+    console.log('Nombre:',req.body.firstName);
+    console.log('Apellido;', req.body.lastName);
+})
+
+app.listen(PORT);
