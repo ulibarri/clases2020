4088fa7f5d9dc97c1e3cecbf369cfb84ff4f6e02
Vanessa Covarrubias <acovarrubias10@ucol.mx>
2021-06-05 00:26:02 -0500

    Practica 15

---
+++ practica 15/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+let app = express();
+let PORT = process.env.PORT || 3000;
+app.use("/assets", express.static(path.join(__dirname + "/public")));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send(
+    `<!DOCTYPE html><html lang="en"><head><link href=assets/style.css type=text/css rel=stylesheet /></head>
+      <body><h1>Hello world</h1></body>
+      <p> Este parrafo debe ser azul </p>
+      </html>`
+  );
+});
+
+app.get("/person/:id", (req, res) => {
+  res.render("person", {
+    name: req.params.id,
+    msg: req.query.msg,
+    times: req.query.times,
+  });
+});
+
+app.get("/student", (req, res) => {
+  res.render("index");
+});
+
+app.post("/student", (req, res) => {
+  res.send(`First name es: ${req.body.fname}, Last name es: ${req.body.lname}`);
+});
+
+app.listen(PORT);
