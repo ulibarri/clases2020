3f2d927290f569c3c1e5b81d91f18b1a07753509
Vanessa Covarrubias <acovarrubias10@ucol.mx>
2021-06-05 00:18:43 -0500

    Practica 14

---
+++ practica 14/practica 14/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let app = express();
+let PORT = process.env.PORT || 3000;
+app.use("/assets", express.static(path.join(__dirname + "/public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send(
+    `<!DOCTYPE html><html lang="en"><head><link href=assets/style.css type=text/css rel=stylesheet /></head>
+      <body><h1>Hello world</h1></body>
+      <p> Este parrafo debe ser azul </p>
+      </html>`
+  );
+});
+
+app.get("/person/:id", (req, res) => {
+  res.send("person", { id: req.params.id, Qstr: req.query.qstr });
+});
+
+app.listen(PORT);
