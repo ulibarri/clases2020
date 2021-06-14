675932fefd74ab3f6d592b5e2356c9624a6b6497
BrandonVG <bvadillo0@ucol.mx>
2021-06-14 18:57:53 -0500

    Practica 18

---
+++ practica_18/src/server.js
@@ -0,0 +1,15 @@
+let express = require("express");
+
+let app = express();
+
+let personsRoute = require("./routes/person");
+
+app.set("view engine","ejs");
+app.use(personsRoute);
+app.use("/assets",express.static(__dirname + "/public"));
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log("esuchando en el puerto 3000");
+});
