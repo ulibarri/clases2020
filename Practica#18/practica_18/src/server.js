45e8ed47c353a4f030d46628341ecd3c277a5dd8
PaAdRaAg <pramos12@hotmail.com>
2021-06-14 20:01:12 -0500

    Practica#18 || Terminada

---
+++ Practica#18/practica_18/src/server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let personsRoute = require('./routes/person');
+
+app.set('view engine', 'ejs');
+app.use(personsRoute);
+app.use('/assets', express.static(__dirname + '/public'));
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("Escuchando en el puerto 3000")
+});
