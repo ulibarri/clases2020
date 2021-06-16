2e3fbf26fd20168510c06bd374e5f6a6f991b886
marioacar <mcarvantes@ucol.mx>
2021-06-16 18:13:03 -0500

    practica18

---
+++ src/server.js
@@ -0,0 +1,15 @@
+let express = require('express');
+
+let app = express();
+
+let personsRoute= require('./routes/person');
+
+app.set('view engine', 'ejs');
+app.use(personsRoute);
+app.use('assets', express.static(__dirname + '/public'));
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log('escuchando en el puerto 300')
+})
