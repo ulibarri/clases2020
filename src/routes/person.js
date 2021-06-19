0cb9d407235e6635718aecf586fa476d43296c0b
IvanJason134 <jestrada1@ucol.mx>
2021-06-19 18:01:38 -0500

    Practica 18 terminada

---
+++ src/routes/person.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/person',(req,res) => {
+    res.send('Has solicitado el listado de personas');
+});
+
+module.exports = router;
