41723dc5144aa8682c86cf851b0561a5c27d2eb8
ErodStar <erodriguez43@ucol.mx>
2021-06-21 22:34:30 -0500

    Añadir práctica #18 y la práctica #19

--- practica_18/src/server.js
+++ practica_18/src/server.js
@@ -1,13 +1,18 @@
   let express = require('express');
 //Inyecion de la dependecia de express
 let app = express();
+let path = require('path');
 //app que hace la funciom de servidor
 let personRoute = require('./routes/person');
 //Incluimos el router que viene en persons
 
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use(personRoute);
 app.use('/assets', express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  res.render('404');
+});
 
 let PORT = process.env.PORT || 3000;
 //definicion del puerto
