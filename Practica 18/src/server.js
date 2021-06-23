bb3585165a59be5787a65e32c5a60e96fbef3b50
XimenaV26 <xvelasco1@ucol.mx>
2021-06-23 18:49:38 -0500

    Practica 19

--- Practica 18/src/server.js
+++ Practica 18/src/server.js
@@ -1,11 +1,17 @@
 let express= require('express');
+let path = require('path');
 let app = express();
 let personsRoute = require('./routes/person');
 
-app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('views', 'ejs');
 app.use(personsRoute);
 app.use('/assets', express.static(__dirname + '/public'));
 
+app.use((req,res,next)=>{
+    res.render('404');
+})
 let PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
