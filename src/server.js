c7363802562946222514391d8289113368822418
Edgar Gonzalez <edgar_gm6@hotmail.com>
2021-06-24 10:26:36 -0500

    handler

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ let personsRoute = require("./routes/person");
 let path = require("path");
 
 app.set("views", path.join(__dirname, "../views"));
-
 app.set("view engine", "ejs");
 app.use(personsRoute);
 app.use("/assets", express.static(path.join(__dirname + "/public")));
+
+app.use((req, res, next) => {
+  res.render("404");
+});
 
 let PORT = process.env.PORT || 3000;
 
