25b4523a2a68a49540216ea4b9d023343e4ab0e3
JuanMichel16 <juan.michel16@outlook.com>
2021-06-24 22:54:50 -0500

    Terminado practica14

---
+++ server.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <link href="assets/style.css" type="text/css" rel="stylesheet"/>
+            </head>
+            <body>
+                <h1>
+                    Hello world!
+                </h1>
+            </body>
+        </html>
+    `);
+});
+
+
+app.get('/person/:id', (req, res) => {
+    res.render(
+            'Person', {
+                ID: req.params.id,
+                message: req.query.message,
+                times: req.query.times
+            });
+});
+
+
+app.listen(port, () => {
+    `El servidor ha arrancado correctamente en el puerto ${port}`;
+});
