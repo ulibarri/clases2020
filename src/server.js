4785a9560e3b88a22cc3d10260c520fe1aec5fc3
martinmt-mx <mmojica0@ucol.mx>
2021-06-26 03:32:08 -0500

    practica 19 terminada

---
+++ src/server.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let app = express();
+let personsRoute = require("./routes/person");
+let path = require("path");
+
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+app.use(personsRoute);
+app.use("/assets", express.static(path.join(__dirname + "/public")));
+
+app.use((req, res, next) => {
+  res.render("404");
+});
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("escuchando en el puerto 3000");
+});
