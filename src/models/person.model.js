ccea71ed5253771c8d3b985ac4e349a7673ac008
alandin1 <alandin1@ucol.mx>
2021-06-26 08:22:03 -0500

    practica19

---
+++ src/models/person.model.js
@@ -0,0 +1,11 @@
+let mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://")
+
+let personSchema = new mongoose.Schema({
+  nombre: String,
+  edad: Number,
+  nacimiento: Date,
+  tipoSangre: String,
+});
+
+module.exports = mongoose.model("Persons", personSchema)
