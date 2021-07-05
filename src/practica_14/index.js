cbcdce46df7f34a87028c66f759122167031d605
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-07-05 08:09:05 -0500

    Practica #15

---
+++ src/practica_14/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (_, res) => {
+  res.render('index');
+});
+app.get('/api', (_, res) => {
+  res.json({ firstName: 'Ulises', lastName: 'Ramirez' });
+});
+app.get('/person/:id', (req, res) => {
+  const ID = req.params.id;
+  const { message, times } = req.query;
+  res.render('person', { ID, message, times });
+});
+app.use('/assets', express.static(__dirname + '/public'));
+app.listen(port, () => console.log('Listening on port', port));
