35bdb7a745b287efbf8f94acb808c9ec196c98d0
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2021-07-05 11:25:21 -0500

    Practica #18

---
+++ src/practica_18/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const personRoutes = require('./routes/person');
+const path = require('path');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/persons', personRoutes);
+app.use('/assets', express.static(path.join(__dirname, 'public')));
+app.listen(3000, () => {
+  console.log('Listen on port 3000');
+});
